45e35ac83e4b66deecd530f3e81d45ebffea5329
vetto <gzzjk159@naver.com>
2023-06-07 11:26:57 +0900

    feat: rightbarbutton 구현

--- VideoRecorder/VideoRecorder/ViewController.swift
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -7,6 +7,25 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        configure()
+    }
+    
+    private func configure() {
+        configureNavigationBar()
+    }
+    
+    private func configureNavigationBar() {
+        let rightButton: UIBarButtonItem = {
+            let button = UIBarButtonItem(image: UIImage(systemName: "video.fill.badge.plus"),
+                                         style: .done,
+                                         target: self,
+                                         action: nil)
+            button.tintColor = .systemIndigo
+            
+            return button
+        }()
+        
+        navigationItem.rightBarButtonItem = rightButton
     }
 }
 
