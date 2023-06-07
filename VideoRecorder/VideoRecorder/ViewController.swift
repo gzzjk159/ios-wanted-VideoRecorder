7834212a87481a3a59a05d0f301b90a355eca826
vetto <gzzjk159@naver.com>
2023-06-07 10:45:39 +0900

    feat: 프로젝트 생성 및 최소 버전 설정

---
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -0,0 +1,12 @@
+//  VideoRecorder - ViewController.swift
+//  created by vetto on 2023/06/06
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
